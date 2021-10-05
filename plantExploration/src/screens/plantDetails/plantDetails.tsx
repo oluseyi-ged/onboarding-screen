15a4ff2040c43e2cea8be29b48fdb87eae17ea2d
royged <oluseyi.ged@gmail.com>
2021-10-05 19:26:52 +0100

    CTA footer button and header implemented in details page

--- plantExploration/src/screens/plantDetails/plantDetails.tsx
+++ plantExploration/src/screens/plantDetails/plantDetails.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import {icons, images, COLORS, SIZES, FONTS} from '../../constants';
 
 const RequirementsBar = ({icon, barPercentage}) => {
@@ -86,8 +87,73 @@ const RequirementDetail = ({icon, label, detail}) => {
   );
 };
 
-const plantDetails = () => {
+const plantDetails = ({navigation}) => {
   //Render
+
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: 10,
+          left: SIZES.padding,
+          right: SIZES.padding,
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1}}>
+            <TouchableOpacity
+              style={{
+                width: 40,
+                height: 40,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 20,
+                backgroundColor: 'rgba(255,255,255,0.5)',
+              }}
+              onPress={() => {
+                navigation.navigate('Home');
+              }}>
+              <Image
+                source={icons.back}
+                resizeMode="contain"
+                style={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}
+            onPress={() => {
+              console.log('Focus on pressed');
+            }}>
+            <Image
+              source={icons.focus}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={{flexDirection: 'row', marginTop: '10%'}}>
+          <View style={{flex: 1}}>
+            <Text style={{color: COLORS.white, ...FONTS.largeTitle}}>
+              Glory Mantas
+            </Text>
+          </View>
+          <View style={{flex: 1}}></View>
+        </View>
+      </View>
+    );
+  }
 
   function renderRequirementsBar() {
     return (
@@ -137,6 +203,71 @@ const plantDetails = () => {
     );
   }
 
+  function renderFooter() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          paddingVertical: SIZES.padding,
+        }}>
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            // width: '50%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderTopRightRadius: 30,
+            borderBottomRightRadius: 30,
+            backgroundColor: COLORS.primary,
+            paddingHorizontal: SIZES.padding,
+          }}
+          onPress={() => {
+            console.log('take action');
+          }}>
+          <Text style={{color: COLORS.white, ...FONTS.h2}}>Take Action</Text>
+          <Image
+            source={icons.chevron}
+            resizeMode="contain"
+            style={{
+              marginLeft: SIZES.padding,
+              width: 20,
+              height: 20,
+            }}
+          />
+        </TouchableOpacity>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingHorizontal: SIZES.padding,
+          }}>
+          <Text
+            style={{
+              flex: 1,
+              color: COLORS.secondary,
+              ...FONTS.h3,
+            }}>
+            Almost 2 weeks of growing time
+          </Text>
+          <Image
+            source={icons.downArrow}
+            resizeMode="contain"
+            style={{
+              tintColor: COLORS.secondary,
+              marginLeft: SIZES.base,
+              width: 20,
+              height: 20,
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       {/* Banner Photo */}
@@ -172,7 +303,11 @@ const plantDetails = () => {
         {renderRequirementsBar()}
 
         {renderRequirements()}
+
+        {renderFooter()}
       </View>
+
+      {renderHeader()}
     </View>
   );
 };
