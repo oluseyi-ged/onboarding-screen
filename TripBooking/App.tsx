d62921dca286f66679c6e83bd2e4b8919190d535
royged <oluseyi.ged@gmail.com>
2021-10-08 05:24:55 +0100

    home header

--- TripBooking/App.tsx
+++ TripBooking/App.tsx
@@ -53,7 +53,35 @@ const App = () => {
         />
 
         {/* Tabs */}
-        <Stack.Screen name="Home" component={Tabs}></Stack.Screen>
+        <Stack.Screen
+          name="Home"
+          component={Tabs}
+          options={{
+            title: null,
+            headerStyle: {backgroundColor: COLORS.white},
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity
+                style={{marginLeft: SIZES.padding}}
+                onPress={onPress}>
+                <Image
+                  source={icons.back}
+                  resizeMode="contain"
+                  style={{width: 25, height: 25}}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity
+                style={{marginRight: SIZES.padding}}
+                onPress={() => console.log('pressed')}>
+                <Image
+                  source={icons.menu}
+                  resizeMode="contain"
+                  style={{width: 25, height: 25}}
+                />
+              </TouchableOpacity>
+            ),
+          }}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
